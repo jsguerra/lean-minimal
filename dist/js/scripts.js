1f179ce48d455399979d90044a67c2d13dc5f6ff
Jose Guerra <jose@gcostudios.com>
2018-08-26 17:20:01 +0100

    updated file

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -6,22 +6,7 @@
  * @version 2.0.0
  * Copyright 2018. MIT licensed.
  */
-(function($) {
-  //comment
-  $(function() {
-    $('nav ul li > a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').slideToggle();
-      $('.nav-dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
-    });
-    $('html').click(function() {
-      $('.nav-dropdown').hide();
-    });
-  });
-  document.querySelector('#nav-toggle').addEventListener('click', function() {
-    this.classList.toggle('active');
-  });
-  $('#nav-toggle').click(function() {
-    $('nav ul').slideToggle();
-  });
-})(jQuery);
+(function (window, document, undefined) {
+  'use strict';
+  // Type your code here
+})(window, document);
