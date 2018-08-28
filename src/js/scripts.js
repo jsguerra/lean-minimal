bf7138b3dfffcfd223cf7525231e7f5fc3e81cfe
Jose Guerra <jose@gcostudios.com>
2018-08-28 21:44:33 +0100

    added mobile side menu toggle

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,13 @@
 (function (window, document, undefined) {
   'use strict';
   
-  const burger = document.querySelector('.hamburger');
+  const burger = document.querySelector('.hamburger'),
+        mobileMenu = document.querySelector('.mobile-nav');
 
   burger.addEventListener('click', function(e) {
     e.preventDefault();
 
-    this.classList.toggle('is-active');
+    this.classList.toggle('is-open');
+    mobileMenu.classList.toggle('is-open');
   });
 })(window, document);
