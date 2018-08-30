c3f1d3fd2658368cf06017f0b72153be02220b06
Jose Guerra <jose.guerra@finalsite.com>
2018-08-30 09:29:45 +0100

    added close function on click outside element

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,10 +4,24 @@
   const burger = document.querySelector('.hamburger'),
         mobileMenu = document.querySelector('.mobile-nav');
 
+  // Burger menu and side menu toggle
   burger.addEventListener('click', function(e) {
     e.preventDefault();
 
     this.classList.toggle('is-open');
     mobileMenu.classList.toggle('is-open');
-  });
+  }, false);
+
+  // Listen for all clicks on the document
+  document.addEventListener('click', function (event) {
+
+    // If the click happened inside the the container, bail
+    if (event.target.closest('.mobile-nav, .hamburger')) {
+      return;
+    } else {
+      // Otherwise, run our code...
+      burger.classList.remove('is-open');
+      mobileMenu.classList.remove('is-open');
+    }
+  }, false);
 })(window, document);
